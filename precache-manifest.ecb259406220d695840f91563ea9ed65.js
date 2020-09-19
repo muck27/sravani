cbec58967d412f9dc6b790e4728db7b35e91a9b5
muck27 <clyde@uncannyvision.com>
2020-09-19 09:49:30 +0530

    Updates

---
+++ precache-manifest.ecb259406220d695840f91563ea9ed65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dad3795861964e7eb586caf9cb5c5e",
+    "url": "/sravani/index.html"
+  },
+  {
+    "revision": "2c6a144592f0560225a5",
+    "url": "/sravani/static/css/main.61ee0b10.chunk.css"
+  },
+  {
+    "revision": "854e5f80ed93d1b962b5",
+    "url": "/sravani/static/js/2.d5b4bb76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sravani/static/js/2.d5b4bb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6a144592f0560225a5",
+    "url": "/sravani/static/js/main.bba9aa51.chunk.js"
+  },
+  {
+    "revision": "7b0254163a78e75f8db3",
+    "url": "/sravani/static/js/runtime-main.9369ed71.js"
+  },
+  {
+    "revision": "9d353d73468320ddc5e8c25308edf7ff",
+    "url": "/sravani/static/media/background.9d353d73.jpg"
+  },
+  {
+    "revision": "b5cb52cae8234019bec276d1f8095302",
+    "url": "/sravani/static/media/logo.b5cb52ca.jpeg"
+  }
+]);
